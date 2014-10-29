8bfd4c43507b07e2982136c26a9d6d014491caf4
ngvhanh <hanh.khtn@yahoo.com>
2014-10-29 15:15:57 +0700

    a

--- video_files/script.js
+++ video_files/script.js
@@ -266,7 +266,7 @@ function sobel(orgFrame)
                      + tempData[nxtLine-4] - tempData[nxtLine+4]);
          // Lấy đạo hàm theo 2 chiều
          var temp =  Math.sqrt(Math.pow(temp1, 2) + Math.pow(temp2, 2));
-
+         alert(temp);
          tempData[i] = temp;
          tempData[i+1] = temp;
          tempData[i+2] = temp;
