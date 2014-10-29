8e3e0ebe98eafa0c715f41ae52996e6037e78534
ngvhanh <hanh.khtn@yahoo.com>
2014-10-29 15:48:54 +0700

    46

--- video_files/script.js
+++ video_files/script.js
@@ -270,7 +270,7 @@ function sobel(orgFrame)
         alert(temp1);
          //alert(temp2);
          //alert(temp);
-         temp1 = Math.sqrt(temp1);
+         temp1 = Math.sqrt(Math.abs(temp1));
          tempData[i] = temp1;
          tempData[i+1] = temp1;
          tempData[i+2] = temp1;
