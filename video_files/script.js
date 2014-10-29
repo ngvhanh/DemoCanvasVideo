52a3e68c50edc17f0079a7fc0f93d7ab841da99e
ngvhanh <hanh.khtn@yahoo.com>
2014-10-29 14:17:48 +0700

    Beta
    Thêm hàm lọc, thêm file process.js

--- video_files/script.js
+++ video_files/script.js
@@ -1,20 +1,23 @@
 // Biến đánh dấu bộ lọc nào sẽ được sử dụng
-// Mặc định là Grayscale
+// Mặc định là Gray
 var filter = 1;
-var DEF_GRAYSCALE = 1;
+var DEF_GRAY = 1;
 var DEF_RED = 2;
 var DEF_GREEN = 3;
 var DEF_BLUE = 4;
 var DEF_SOBEL = 5;
-var DEF_GAUSS = 6;
-var DEF_AVERAGE = 7;
+var DEF_PREWITT = 6
+var DEF_LAPLACE = 7;
+var DEF_AVERAGE = 8;
 var DEF_MAX = DEF_AVERAGE;
-var TXT_GRAYSCALE = "Grayscale"
+
+var TXT_GRAY = "Gray"
 var TXT_RED = "Red"
 var TXT_GREEN = "Green"
 var TXT_BLUE = "Blue"
 var TXT_SOBEL = "Sobel"
-var TXT_GAUSS = "Gauss"
+var TXT_PREWITT = "Prewitt"
+var TXT_LAPLACE = "Laplace"
 var TXT_AVERAGE = "Average"
 
 var DEF_ADD = 50;
@@ -67,20 +70,22 @@ function draw(video, context, width, height, filterType)
     resultFrame = orgFrame;
 
     // Xử lý dựa vào bộ lọc hiện tại
-    //Grayscale
-    if(filterType == DEF_GRAYSCALE)
-        resultFrame = grayscale(orgFrame);
-    if(filterType == DEF_RED)
+    //Gray
+    if(filterType == DEF_GRAY)
+        resultFrame = gray(orgFrame);
+    else if(filterType == DEF_RED)
         resultFrame = red(orgFrame);
-    if(filterType == DEF_GREEN)
+    else if(filterType == DEF_GREEN)
         resultFrame = green(orgFrame);
-    if(filterType == DEF_BLUE)
+    else if(filterType == DEF_BLUE)
         resultFrame = blue(orgFrame);
-    if(filterType == DEF_SOBEL)
-        resultFrame = edgeDetect(orgFrame);
-    if(filterType == DEF_GAUSS)
+    else if(filterType == DEF_SOBEL)
+        resultFrame = sobel(orgFrame);
+    else if(filterType == DEF_PREWITT)
+        resultFrame = prewitt(orgFrame);
+    else if(filterType == DEF_LAPLACE)
         resultFrame = gaussBlur(orgFrame);
-    if(filterType == DEF_AVERAGE)
+    else if(filterType == DEF_AVERAGE)
         resultFrame = average(orgFrame);
     
     // Vẽ lại kết quả xử lý lên canvas thể hiện video đích
@@ -90,204 +95,31 @@ function draw(video, context, width, height, filterType)
     setTimeout(draw, 40, video, context, width, height, filter); 
 }
 
-// ---------------- CÁC HÀM XỬ LÝ VIDEO THEO CÁC BỘ LỌC --------------
-
-// Nhận vào một frame và trả về video frame dưới dạng grayscale
-function grayscale(orgFrame)
-{;
-    var resultFrame = orgFrame;
-    var tempData = resultFrame.data;
-    
-    // Duyệt qua từng pixel và chuyển pixel đó sang dạng grayscale
-    var length = tempData.length;
-    for (var i = 0; i < length; i += 4)
-    {                    
-        var brightness = 0.2126 * tempData[i] + 0.7152 * tempData[i+1] + 0.0722 * tempData[i+2];
-        tempData[i] = brightness;
-        tempData[i+1] = brightness;
-        tempData[i+2] = brightness;
-        tempData[i+3] = 255;
-    }
-    resultFrame.data = tempData;
-
-    return resultFrame;
-}
-
-// Nhận vào một frame và trả về video frame được thêm màu ở kênh Red
-function red(orgFrame)
-{
-    var resultFrame = orgFrame;
-    var tempData = resultFrame.data;
-    
-    // Duyệt qua từng pixel và cộng thêm vào kênh red
-    var length = tempData.length;
-    for (var i = 0; i < length; i += 4)
-    {                    
-        var temp = tempData[i] + DEF_ADD;
-        if(temp > 225)
-            temp = 225;
-        tempData[i] = temp;
-    }
-
-    resultFrame.data = tempData;
-
-    return resultFrame;
-}
-
-
-// Nhận vào một frame và trả về video frame được thêm màu ở kênh Green
-function green(orgFrame)
-{
-    var resultFrame = orgFrame;
-    var tempData = resultFrame.data;
-    
-    // Duyệt qua từng pixel và cộng thêm vào kênh green
-    var length = tempData.length;
-    for (var i = 0; i < length; i += 4)
-    {                    
-        var temp = tempData[i+1] + DEF_ADD;
-        if(temp > 225)
-            temp = 225;
-        tempData[i+1] = temp;
-    }
-
-    resultFrame.data = tempData;
-
-    return resultFrame;
-}
-
-
-// Nhận vào một frame và trả về video frame được thêm màu ở kênh Blue
-function blue(orgFrame)
-{
-    var resultFrame = orgFrame;
-    var tempData = resultFrame.data;
-    
-    // Duyệt qua từng pixel và cộng thêm vào kênh blue
-    var length = tempData.length;
-    for (var i = 0; i < length; i += 4)
-    {                    
-        var temp = tempData[i+2] + DEF_ADD;
-        if(temp > 225)
-            temp = 225;
-        tempData[i+2] = temp;
-    }
-
-    resultFrame.data = tempData;
-
-    return resultFrame;
-}
-
-// Nhận vào một frame và trả về video frame được lọc trung bình
-function average(orgFrame)
-{
-    var resultFrame = orgFrame;
-    var tempData = resultFrame.data;
-    
-    // Duyệt qua từng pixel và gán bằng giá trị trung bình của các pixel xung quanh
-    var width = orgFrame.width;
-    // var height = orgFrame.height;
-    // for(var w = 1; w < width - 1; w++)
-    // {                    
-    //     for(var h = 1; h < height - 1; h++)
-    //     {
-    //         var pos = width * height * 4;
-    //         var new0 = tempData[i] 
-    //     }
-    // }
-
-    var length = tempData.length;
-    for(var i = width; i < length - width; i++)
-    {
-        var prvLine = i - width * 4;
-        var nxtLine = i - width * 4;
-        tempData[i] = (tempData[prvLine - 4] + tempData[prvLine] + tempData[prvLine + 4]
-                        + tempData[i - 4] + tempData[i] + tempData[i + 4]
-                        + tempData[nxtLine - 4] + tempData[nxtLine] + tempData[nxtLine + 4]) / 9;
-    }
-
-    resultFrame.data = tempData;
-
-    return resultFrame;
-}
-
-// Hàm thực hiện tính tích chập với các kernal
-function convolute(iData, kernel, delta)
-{
-    var w = iData.width;
-    var pData = iData.data;
-    var rData = iData.data;
-    var length = pData.length;
-    var i;
-    var step = w*4;
-    
-    for (i=0; i<length; i++)
-    {
-        if(i%4 == 3)
-        {
-            continue;
-        }       
-        
-        iPrev = i - step;
-        iNext = i + step;
-        pData[i] = rData[iPrev-4]*kernel[8] + rData[iPrev]*kernel[7] + rData[iPrev+4]*kernel[6]
-                    + rData[i-4]*kernel[5] + rData[i]*kernel[4] + rData[i+4]*kernel[3]
-                    + rData[iNext-4]*kernel[2] + rData[iNext]*kernel[1] + rData[iNext+4]*kernel[0] + delta;
-    }    
-    iData.data = pData;
-}
-
-//Edge detection
-function edgeDetect(frameData)
-{
-    var iData = frameData;
-    
-    var kernel = [0, -1, 0,
-                 -1, 2, 0,
-                 0, 0, 0];
-    
-    convolute(iData, kernel, 100);
-    
-    return iData;
-}
-
-//Gaussian blur
-function gaussBlur(frameData)
-{
-    var iData = frameData;
-    
-    var kernel = [0.0625, 0.125, 0.0625,
-                 0.125, 0.25, 0.125,
-                 0.0625, 0.125, 0.0625];
-    
-    convolute(iData, kernel, 0);
-    
-    return iData;
-}
 
 //Change filter
 function nextFilter()
 {
-    var fTxt = document.getElementById('filterText');
+    var filterText = document.getElementById('afterText');
     
     filter++;
     if(filter > DEF_MAX)
-        filter = DEF_GRAYSCALE;
+        filter = DEF_GRAY;
 
-    if(filter == DEF_GRAYSCALE)
-        fTxt.innerHTML = TXT_GRAYSCALE;
-    if(filter == DEF_RED)
-        fTxt.innerHTML = TXT_RED;
-    if(filter == DEF_GREEN)
-        fTxt.innerHTML = TXT_GREEN;
-    if(filter == DEF_BLUE)
-        fTxt.innerHTML = TXT_BLUE;
-    if(filter == DEF_SOBEL)
-        fTxt.innerHTML = TXT_SOBEL;
-    if(filter == DEF_GAUSS)
-        fTxt.innerHTML = TXT_GAUSS;
-    if(filter == DEF_AVERAGE)
-        fTxt.innerHTML = TXT_AVERAGE;
-
-    
+    if(filter == DEF_GRAY)
+        filterText.innerHTML = TXT_GRAY;
+    else if(filter == DEF_RED)
+        filterText.innerHTML = TXT_RED;
+    else if(filter == DEF_GREEN)
+        filterText.innerHTML = TXT_GREEN;
+    else if(filter == DEF_BLUE)
+        filterText.innerHTML = TXT_BLUE;
+    else if(filter == DEF_SOBEL)
+        filterText.innerHTML = TXT_SOBEL;
+    else if(filter == DEF_PREWITT)
+        filterText.innerHTML = TXT_PREWITT;
+    else if(filter == DEF_LAPLACE)
+        filterText.innerHTML = TXT_LAPLACE;
+    else if(filter == DEF_AVERAGE)
+        filterText.innerHTML = TXT_AVERAGE;
+   
 }
